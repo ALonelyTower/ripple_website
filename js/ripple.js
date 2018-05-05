9a60852909030297b83df53060837a7fd8fd9648
Zenarim <jlwong092014@gmail.com>
2018-05-05 01:01:06 -0700

    Add page for signing created transactions

--- js/ripple.js
+++ js/ripple.js
@@ -5,20 +5,32 @@ function generateKeys() {
   document.getElementById("private_key").innerHTML = account.secret;
 }
 
-
 function createTransaction() {
     _createTransaction().then(function(txJSON){
         var transaction = JSON.parse(txJSON['txJSON']);
         document.getElementById("create_transaction_transtype").innerHTML = "Transaction Type: " + transaction.TransactionType;
         document.getElementById("create_transaction_account").innerHTML = "Account: " + transaction.Account;
         document.getElementById("create_transaction_destination").innerHTML = "Destination: " + transaction.Destination;
-        document.getElementById("create_transaction_amount").innerHTML = "Amount: " + transaction.Amount;
+        document.getElementById("create_transaction_amount").innerHTML = "Amount: " + transaction.Amount + " drops (1000000 drops per 1 XRP)";
         document.getElementById("create_transaction_flags").innerHTML = "Flags: " + transaction.Flags;
         document.getElementById("create_transaction_lastledgersequence").innerHTML = "Last Ledger Sequence: " + transaction.LastLedgerSequence;
         document.getElementById("create_transaction_fee").innerHTML = "Fee: " + transaction.Fee;
-        document.getElementById("create_transaction_sequences").innerHTML = "Sequences: " + transaction.Sequences;
-
+        document.getElementById("create_transaction_sequences").innerHTML = "Sequences: " + transaction.Sequence;
+        document.getElementById("create_transaction_raw_txjson_object").innerHTML = txJSON['txJSON'];
     }).catch(console.error);
+}
+
+function signTransaction() {
+    _signTransaction().then(function(signed_tx_object){
+        document.getElementById("signed_transfer_object").innerHTML = signed_tx_object;
+    }).catch(console.error);
+}
+
+function _signTransaction() {
+    var secret_key = document.getElementById("sign_transaction_secret_key");
+    var txjson_object = document.getElementById("sign_transaction_txjson_object");
+    var api = new ripple.RippleAPI();
+    return api.sign(txjson_object, secret_key);
 }
 
 function _createTransaction() {
@@ -29,7 +41,6 @@ function _createTransaction() {
     const instructions = createInstructions();
     var api = new ripple.RippleAPI();
     return api.preparePayment(send_address, payment, instructions);
-    // return api.preparePayment(send_address, payment);
 }
 
 function createPayment(source_address, destination_address, xrp_amount) {
@@ -50,7 +61,7 @@ function createPayment(source_address, destination_address, xrp_amount) {
 function createInstructions() {
     var instructions = {};
     instructions['fee'] = "0.00001";
-    instructions['sequence'] = 1;
+    instructions['sequence'] = 10;
     instructions['maxLedgerVersion'] = 53421;
     return instructions;
 }
